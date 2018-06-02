fe148503ddb614476ac954633f7c0cf1e58a4f8c
Vincensius Augustino <39743055+vincensiusaug@users.noreply.github.com>
2018-06-02 19:19:07 +0700

    Best Hanoi Ever

--- no-04.py
+++ no-04.py
@@ -11,27 +11,38 @@ class Hanoi:
             try:
                 print ("{0:^3}|".format(self.arrA[i]),end="")
             except IndexError:
-                print ("{0:^3}|".format("-"),end="")
+                print ("{0:^3}|".format(" "),end="")
 
             try:
                 print ("{0:^3}|".format(self.arrB[i]),end="")
             except IndexError:
-                print ("{0:^3}|".format("-"),end="")
+                print ("{0:^3}|".format(" "),end="")
 
             try:
                 print ("{0:^3}".format(self.arrC[i]))
             except IndexError:
-                print ("{0:^3}".format("-"))
+                print ("{0:^3}".format(" "))
 
         print ("{0:=<11}".format(""))
+        print ("{0:^3}|{1:^3}|{2:^3}".format("A", "B", "C"))
 
     def moveNumber(self, beg, end):
         number = beg.pop()
         end.append(number)
         if beg == self.arrA:
-            a = 1
+            a = "A"
+        elif beg == self.arrB:
+            a = "B"
+        elif beg == self.arrC:
+            a = "C"
+        if end == self.arrA:
+            b = "A"
+        elif end == self.arrB:
+            b = "B"
+        elif end == self.arrC:
+            b = "C"
         print ()
-        print ("{0:} - {1:} -> {2:}".format(number, beg, end))
+        print ("Moving {0:}   from peg {1:} -> peg {2:}\n".format(number, a, b))
         self.out()
 
     def move(self, n = None, beg = None, mid = None, end = None):
@@ -39,18 +50,13 @@ class Hanoi:
 
         if n == None:
             n = self.number
-        if beg == None:
             beg = self.arrA
-        if mid == None:
             mid = self.arrB
-        if end == None:
             end = self.arrC
 
-        if n==1:
-            self.moveNumber(beg, end)
-        else:
+        if n>0:
             move(n-1, beg, end, mid)
-            move(1, beg, mid, end)
+            self.moveNumber(beg, end)
             move(n-1, mid, beg, end)
 
 tower = Hanoi(3)
