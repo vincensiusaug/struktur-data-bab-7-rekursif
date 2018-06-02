8c845878c010a397ca315900befc157e3c6d4eaf
Vincensius Augustino <39743055+vincensiusaug@users.noreply.github.com>
2018-06-03 05:09:49 +0700

    quicksort 3 rasa

--- no-01.py
+++ no-01.py
@@ -2,65 +2,163 @@ import time
 import random
 
 class Sort:
-    def __init__(self, amount):
-        self.count = 0
-        self.data = []
-        self.data_assign(amount)
+    count = 0
+
+    def __init__(self, model, data = None):
+        self.model = model
+        if data == None:
+            self.data = []
     
     def data_assign(self, amount):
-        if amount > 0:
+        if amount > 1:
             self.data_assign(amount-1)
         self.data.append(random.randint(1, 99))
 
-    def partition(self, start, end):
-        pivot = start
+    def partition1(self, start, end):
+        data = self.data[start:end+1]
 
-        def comparator(i, end, pivot):
-            if i < end:
+        pivot = int(len(data)/2)
+
+        pivot_num = data[pivot]
+        data_1 = data[:pivot]
+        data_2 = data[pivot+1:]
+    
+        def comparator_1(indeks = 0):
+            if indeks < len(data_1):
                 self.count += 1
-                if self.data[i] < self.data[end]:
-                    self.data[i], self.data[pivot] = self.data[pivot], self.data[i]
-                    pivot += 1
-                pivot = comparator(i+1, end, pivot)
-            return pivot
-
-        pivot = comparator(start, end, pivot)
-
-        # for i in range(start, end):
-        #     if self.data[i] < self.data[end]:
-        #         self.data[i], self.data[pivot] = self.data[pivot], self.data[i]
-        #         pivot += 1
+                if data_1[indeks] > pivot_num:
+                    data_2.append(data_1.pop(indeks))
+                    comparator_1(indeks)
+                else:
+                    comparator_1(indeks+1)
         
-        self.data[pivot], self.data[end] = self.data[end], self.data[pivot]
+        def comparator_2(indeks = 0):
+            if indeks < len(data_2):
+                self.count += 1
+                if data_2[indeks] < pivot_num:
+                    data_1.append(data_2.pop(indeks))
+                    comparator_2(indeks)
+                else:
+                    comparator_2(indeks+1)
 
+        comparator_1()
+        comparator_2()
+
+        pivot = len(data_1)
+        data_1.append(pivot_num)
+        data = data_1 + data_2
+
+        self.data[start:end+1] = data
+        return pivot
+
+    def partition2(self, start, end):
+        data = self.data[start:end+1]
+
+        pivot = 0
+        
+        pivot_num = data[pivot]
+        data_1 = data[:pivot]
+        data_2 = data[pivot+1:]
+    
+        def comparator_1(indeks = 0):
+            if indeks < len(data_1):
+                self.count += 1
+                if data_1[indeks] > pivot_num:
+                    data_2.append(data_1.pop(indeks))
+                    comparator_1(indeks)
+                else:
+                    comparator_1(indeks+1)
+        
+        def comparator_2(indeks = 0):
+            if indeks < len(data_2):
+                self.count += 1
+                if data_2[indeks] < pivot_num:
+                    data_1.append(data_2.pop(indeks))
+                    comparator_2(indeks)
+                else:
+                    comparator_2(indeks+1)
+
+        comparator_1()
+        comparator_2()
+
+        pivot = len(data_1)
+        data_1.append(pivot_num)
+        data = data_1 + data_2
+
+        self.data[start:end+1] = data
+        return pivot
+
+    def partition3(self, start, end):
+        data = self.data[start:end+1]
+
+        pivot = len(data)-1
+        
+        pivot_num = data[pivot]
+        data_1 = data[:pivot]
+        data_2 = data[pivot+1:]
+    
+        def comparator_1(indeks = 0):
+            if indeks < len(data_1):
+                self.count += 1
+                if data_1[indeks] > pivot_num:
+                    data_2.append(data_1.pop(indeks))
+                    comparator_1(indeks)
+                else:
+                    comparator_1(indeks+1)
+        
+        def comparator_2(indeks = 0):
+            if indeks < len(data_2):
+                self.count += 1
+                if data_2[indeks] < pivot_num:
+                    data_1.append(data_2.pop(indeks))
+                    comparator_2(indeks)
+                else:
+                    comparator_2(indeks+1)
+
+        comparator_1()
+        comparator_2()
+
+        pivot = len(data_1)
+        data_1.append(pivot_num)
+        data = data_1 + data_2
+
+        self.data[start:end+1] = data
         return pivot
 
     def quick(self, start, end):
         if start < end:
-            pivot = self.partition(start, end)
+            if self.model == 1:
+                pivot = start + self.partition1(start, end)
+            elif self.model == 2:
+                pivot = start + self.partition2(start, end)
+            else:
+                pivot = start + self.partition3(start, end)
             self.quick(start, pivot-1)
             self.quick(pivot+1, end)
 
     def quicksort(self):
-        self.waktu = time.time()
+        self.waktu = time.clock()
         self.quick(0, len(self.data) - 1)
-        self.waktu = time.time() - self.waktu
+        self.waktu = time.clock() - self.waktu
 
-    def efective_time(self):
-        return ("{0:.4}".format(self.waktu*1000))
 
-    def efective_count(self):
-        return self.count
+data_total = int(input("Masukan jumlah data = "))
+class_total = 1000
 
-jumlah = int(input("Masukan jumlah data = "))
+for model in range(1, 4):
+    quick = []
+    time_total = 0
+    step_total = 0
+    for i in range(class_total):
+        quick.append(Sort(model))
 
-quick = []
-total = 0
+        quick[i].data_assign(data_total)
+        quick[i].quicksort()
 
-for i in range(10):
-    quick.append(Sort(jumlah))
-    quick[i].quicksort()
-    total += float(quick[i].efective_time())
+        time_total += quick[i].waktu
+        step_total += quick[i].count
 
-total = total/jumlah
-print ("Waktu rata-rata dari {0:} data adalah {1:.4}ms".format(jumlah, total))
+    time_total = time_total/class_total
+    step_total = step_total/class_total
+
+    print ("\nModel {0:}\nWaktu =  {1:.6f} detik\nLangkah =  {2:.0f}".format(model, time_total, step_total))
