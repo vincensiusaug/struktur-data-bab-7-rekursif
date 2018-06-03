37caedb52f3082a27b7563a87494475c1ccde103
Vincensius Augustino <39743055+vincensiusaug@users.noreply.github.com>
2018-06-03 14:38:14 +0700

    NchooseK dengan tabel

--- no-05.py
+++ no-05.py
@@ -1,3 +1,4 @@
+
 def fact(n):
     if n>0:
         return n*fact(n-1)
@@ -7,4 +8,25 @@ def fact(n):
 def nchoosek(n, k):
     return fact(n)/(fact(k)*fact(n-k))
 
-print (nchoosek(5,2))
+def n_table(n, k, f = 1):
+    if f:
+        print ()
+        print ("|{0:=^16}|".format(""))
+        print ("|{0:^3}|{1:^3}|{2:^8}|".format("N", "K", "Result"))
+        print ("|{0:=^16}|".format(""))
+    if n > 0:
+        def k_table(k):
+            if k > 0:
+                if k >= n:
+                    k_table(k-1)
+                else:
+                    print ("|{0:^3}|{1:^3}|{2:^8.0f}|".format(n, k, nchoosek(n, k)))
+                    k_table(k-1)
+        
+        k_table(k)
+        n_table(n-1, k, 0)
+    else:
+        print ("|{0:=^16}|".format(""))
+        print ()
+
+n_table(20,13)
